7227c4d3f4c90ba755c72c5d1aea5770ebce84cc
Alex <deadlynjdj@gmail.com>
2021-10-24 20:00:23 -0400

    fixed IFTTT being a dev is not fun

--- src/services/statuspage/ifttt.ts
+++ src/services/statuspage/ifttt.ts
@@ -1,6 +1,6 @@
 import StatusPageService from '../base/statuspage'
 
-class Hubspot extends StatusPageService {
+class IFTTT extends StatusPageService {
   constructor() {
     super('IFTTT', 'https://status.ifttt.com', 'b8h007xb5lsy')
   }
