7d3f5c476127e5c188c4636c90d67b105c5147a7
Alex <deadlynjdj@gmail.com>
2022-01-27 20:51:16 -0500

    fix

--- src/services/statuspage/redis.ts
+++ src/services/statuspage/redis.ts
@@ -1,6 +1,6 @@
 import StatusPageService from '../base/statuspage'
 
-class Reddit extends StatusPageService {
+class Redis extends StatusPageService {
   constructor() {
     super('Redis', 'https://status.redis.com/', 'j6ydyp75k0zx')
   }
