5b62dee25a02ad81339e3b3ff45ca5955f0303d3
Alex <44127001+deadlynjdj@users.noreply.github.com>
2025-03-30 20:09:27 -0400

    refactor: update useInterval to use TypeScript types for callback and delay

--- src/utils/use-interval.ts
+++ src/utils/use-interval.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react'
 
-const useInterval = (callback, delay) => {
-  const savedCallback = useRef()
+const useInterval = (callback: () => void, delay: number | null) => {
+  const savedCallback = useRef<() => void>()
 
   useEffect(() => {
     savedCallback.current = callback
